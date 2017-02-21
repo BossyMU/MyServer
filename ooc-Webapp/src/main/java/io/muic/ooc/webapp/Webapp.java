c204518f43bd1cfd2d74bc6c48096bd197c788e7
Bossy <torpong.boss@gmail.com>
2017-02-21 19:25:32 +0700

    fouth

--- ooc-Webapp/src/main/java/io/muic/ooc/webapp/Webapp.java
+++ ooc-Webapp/src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -19,7 +19,7 @@ public class Webapp {
 
         String docBase = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8082);
+        tomcat.setPort(80);
         SecurityService securityService = new SecurityService();
 
         ServletRouter servletRouter = new ServletRouter();
